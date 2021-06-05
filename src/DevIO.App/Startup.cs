146d596b31d0e9645a33ba87111faf6e3ee7eab9
Dlucap <dlpsilva@yahoo.com.br>
2021-06-05 12:07:23 -0300

    Criado acesso ao banco via repositórios Ajuste nome propriedade Endereco MeuDbContext

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -1,18 +1,14 @@
 using DevIO.App.Data;
+using DevIO.Business.Interfaces;
 using DevIO.Data.Context;
+using DevIO.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevIO.App
 {
@@ -40,6 +36,11 @@ namespace DevIO.App
       services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
           .AddEntityFrameworkStores<ApplicationDbContext>();
       services.AddControllersWithViews();
+
+      services.AddScoped<MeuDbContext>();
+      services.AddScoped<IProdutoRepository, ProdutoRepository>(); 
+      services.AddScoped<IFornecedorRepository, FornecedorRepository>();  
+      services.AddScoped<IEnderecoRepository, IEnderecoRepository>(); 
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/DevIO.Business/Models/Fornecedor.cs
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AppMvcBasica.Models
 {
@@ -23,6 +18,4 @@ namespace AppMvcBasica.Models
     public IEnumerable<Produto> Produtos { get; set; }
 
   }
-
-
 }
